594f6c7b6cb36ca4758a80fd7729440058c20132
Kieran-Shaw <71037357+Kieran-Shaw@users.noreply.github.com>
2023-07-25 07:17:56 -0400

    Create virtual environment, requirements.txt file and set up sample endpoints

---
+++ main.py
@@ -0,0 +1,40 @@
+from flask import Flask, jsonify
+import os
+import logging
+
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__)
+    
+    # setup logging
+    logging.basicConfig(level=logging.DEBUG)
+    
+    @app.route('/')
+    def hello_world():
+        app.logger.info('Processing default request')
+        return 'Hello, World!'
+
+    @app.route('/endpoint1')
+    def endpoint1():
+        return 'This is endpoint 1.'
+
+    @app.route('/endpoint2')
+    def endpoint2():
+        return 'This is endpoint 2.'
+    
+    @app.errorhandler(404)
+    def page_not_found(e):
+        app.logger.error('Page not found')
+        return jsonify(error=str(e)), 404
+
+    @app.errorhandler(500)
+    def internal_server_error(e):
+        app.logger.error('Server error')
+        return jsonify(error=str(e)), 500
+
+    return app
+
+app = create_app()
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=int(os.getenv('PORT', '8080')))
